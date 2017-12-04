aa081f0cc9deb1057e7c795416078a50ab3ba505
Hector Hurtarte <hectorh30@gmail.com>
2017-12-04 00:37:39 -0600

    Update build-data.js script to separated spreadsheet urls

--- build-data.js
+++ build-data.js
@@ -1,12 +1,27 @@
+/**
+ * Usage: node build-data.js
+ */
+
 const Tabletop = require('tabletop');
 const jsonfile = require('jsonfile');
 const fs = require('fs');
 
-var spreadsheet = fs.readFileSync('public/spreadsheet-url', 'utf-8').trim();
 var targetPath = 'public/static-files/';
 
 Tabletop.init({
-  key: spreadsheet,
+  key: fs.readFileSync('public/data-spreadsheet-url', 'utf-8').trim(),
+  callback: function(sheets) {
+    Object.keys(sheets).forEach(function(key) {
+      var file = targetPath + key + '.json';
+      
+      jsonfile.spaces = 2;
+      jsonfile.writeFileSync(file, sheets[key].elements);
+    });
+  }
+});
+
+Tabletop.init({
+  key: fs.readFileSync('public/config-spreadsheet-url', 'utf-8').trim(),
   callback: function(sheets) {
     Object.keys(sheets).forEach(function(key) {
       var file = targetPath + key + '.json';
