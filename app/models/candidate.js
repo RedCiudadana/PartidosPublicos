119488e7251a54c2d0df5ac3455244c83c838dcb
D3AH <diegoeauyonh@gmail.com>
2018-11-21 09:33:41 -0600

    Agregando modelo base "candidate".

---
+++ app/models/candidate.js
@@ -0,0 +1,116 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { computed } from '@ember/object';
+
+/**
+ * Modelo base para candidatos.
+ *
+ * @class Model.Candidate
+ */
+export default Model.extend({
+  // Attributes
+
+  /**
+   * Nombre del candidato
+   *
+   * @property nombre
+   * @type String
+   */
+  nombre: attr('string'),
+
+  /**
+   * Id del partido
+   *
+   * @property partido
+   * @type Number
+   */
+  partido: attr('number'),
+
+  /**
+   * edad
+   *
+   * @property edad
+   * @type Number
+   */
+  edad: attr('number'),
+
+  /**
+   * estadoCivil
+   *
+   * @property estadoCivil
+   * @type String
+   */
+  estadoCivil: attr('string'),
+
+  /**
+   * anosProfesional
+   *
+   * @property anosProfesional
+   * @type Number
+   */
+  anosProfesional: attr('number'),
+
+  /**
+   * numeroColegiado
+   *
+   * @property numeroColegiado
+   * @type Number
+   */
+  numeroColegiado: attr('number'),
+
+  /**
+   * sexo
+   *
+   * @property sexo
+   * @type String
+   */
+  sexo: attr('string'),
+
+  /**
+   * fb
+   *
+   * @property fb
+   * @type String
+   */
+  fb: attr('string'),
+
+  /**
+   * tw
+   *
+   * @property tw
+   * @type String
+   */
+  tw: attr('string'),
+
+  /**
+   * fotoUrl
+   *
+   * @property fotoUrl
+   * @type String
+   */
+  fotoUrl: attr('string'),
+
+  // Computed properties
+
+  /**
+   * Foto del perfil, en el caso que fotoURL este vacío se verifica el sexo y se establece una imagen por defecto respectivamente.
+   *
+   * @param fotoURL String enlace de la foto
+   * @param sexo Sexo del candidato.
+   * @return String enlace de la foto del candidato
+   */
+  fotoPerfil: computed('fotoUrl', 'sexo', function() {
+    if (this.get('fotoUrl') !== '') {
+      return this.get('fotoUrl');
+    }
+
+    if (this.get('sexo') === 'Masculino') {
+      return 'mi-guatemala/img/candidato.png';
+    }
+
+    if (this.get('sexo') === 'Femenino') {
+      return 'mi-guatemala/img/candidata.png';
+    }
+  }),
+
+});
