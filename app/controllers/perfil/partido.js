8a71b379a632c08b8a61e090106c7bb2194b94a0
D3AH <diegoeauyonh@gmail.com>
2018-12-14 10:45:49 -0600

    Agregando paginación en sección de miembros en perfil/partido.

---
+++ app/controllers/perfil/partido.js
@@ -0,0 +1,33 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
+
+export default Controller.extend({
+
+  // Pagination
+
+  // setup our query params
+  queryParams: ["page", "perPage"],
+
+  // set default values, can cause problems if left out
+  // if value matches default, it won't display in the URL
+  page: 1,
+  perPage: 10,
+
+  // can be called anything, I've called it pagedContent
+  // remember to iterate over pagedContent in your template
+  pagedContent: pagedArray('model.miembros', {
+    page: computed.alias("parent.page"),
+    perPage: computed.alias("parent.perPage")
+  }),
+
+  // binding the property on the paged array
+  // to a property on the controller
+  totalPages: computed.oneWay("pagedContent.totalPages"),
+
+  actions: {
+    applyFilter() {
+      return this._applyFilter();
+    }
+  }
+});
---
+++ tests/unit/controllers/perfil/partido-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | perfil/partido', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:perfil/partido');
+    assert.ok(controller);
+  });
+});
