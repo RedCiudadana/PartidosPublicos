fb61ee58813afe32c710bf39470b4c6a4173d7b7
D3AH <diegoeauyonh@gmail.com>
2019-01-07 10:03:20 -0600

    Agregando /frente-a-frente.

--- app/routes/frente-a-frente.js
+++ app/routes/frente-a-frente.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { hash } from 'rsvp';
+import { A } from '@ember/array';
 
 export default Route.extend({
 
@@ -8,8 +9,14 @@ export default Route.extend({
 
   model() {
     const spreadsheet = this.get('spreadsheets');
+    let modelData = A();
+    let app = this.modelFor('application');
+    modelData.pushObjects(app.presidents.toArray());
+    modelData.pushObjects(app.deputies.toArray());
+    modelData.pushObjects(app.parlacens.toArray());
+    modelData.pushObjects(app.mayors.toArray());
     return hash({
-      profiles: this.modelFor('index'),
+      profiles: modelData,
       info: spreadsheet.fetch('info-president'),
       historial: spreadsheet.fetch('historial')
     });
@@ -17,7 +24,7 @@ export default Route.extend({
 
   setupController(controller, model) {
     this._super(controller, model);
-    this.controllerFor('frente-a-frente').set('perfilUno', this.modelFor('index').firstObject);
-    this.controllerFor('frente-a-frente').set('perfilDos', this.modelFor('index').firstObject);
+    this.controllerFor('frente-a-frente').set('perfilUno', model.profiles.firstObject);
+    this.controllerFor('frente-a-frente').set('perfilDos', model.profiles.firstObject);
   }
 });
