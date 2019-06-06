05f568735f192d8ecd16bfc16573591243cd5727
d3ah <diegoeauyonh@gmail.com>
2019-06-05 21:35:59 -0600

    Mostrar casilla en candidatos por parlacen.

--- app/models/parlacen.js
+++ app/models/parlacen.js
@@ -1,4 +1,5 @@
 import Candidate from './candidate';
+import attr from 'ember-data/attr';
 
 /**
  * Modelo para candidatos a diputos de parlacen
@@ -8,6 +9,7 @@ import Candidate from './candidate';
  */
 export default Candidate.extend({
   // Atributes
+  casilla: attr('string'),
 
   // Tipo de elección
   type: 'parlacen',
