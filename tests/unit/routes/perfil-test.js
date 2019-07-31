ae37201523d62aaca30430cb5a243d7808743c79
d3h <diegoeauyonh@gmail.com>
2019-07-31 02:37:44 -0600

    Create default unit tests

--- tests/unit/routes/perfil-test.js
+++ tests/unit/routes/perfil-test.js
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
 
-describe('Unit | Route | perfiles/index', function() {
+describe('Unit | Route | perfil', function() {
   setupTest();
 
   it('exists', function() {
-    let route = this.owner.lookup('route:perfiles/index');
+    let route = this.owner.lookup('route:perfil');
     expect(route).to.be.ok;
   });
 });
