75171efe5dd180a45be26e1521715e5b4bad6309
d3h <diegoeauyonh@gmail.com>
2019-11-22 00:50:33 -0600

    Actualizar iconos perfil y agregar orgchart component
    Se actualizaron iconos en perfil.
    
    Se agrego la libreria orgchart.js con ember-auto-import.
    Se instalo el addon ember-auto-import.
    Se agrego un component wrapper de orgchart de prueba.

---
+++ app/components/orgchart.js
@@ -0,0 +1,45 @@
+import Component from '@ember/component';
+import OrgChart from 'orgchart.js';
+
+export default Component.extend({
+  didRender() {
+    this._super(...arguments);
+
+    let datascource = {
+      'name': 'Entidad',
+      'title': 'Puesto',
+      'children': [
+        { 'name': 'Entidad', 'title': 'Puesto' },
+        { 'name': 'Entidad', 'title': 'Puesto',
+          'children': [
+            { 'name': 'Entidad', 'title': 'Puesto' },
+            { 'name': 'Entidad', 'title': 'Puesto',
+              'children': [
+                { 'name': 'Entidad', 'title': 'Puesto' },
+                { 'name': 'Entidad', 'title': 'Puesto',
+                  'children': [
+                    { 'name': 'Entidad', 'title': 'Puesto' },
+                    { 'name': 'Entidad', 'title': 'Puesto' }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        { 'name': 'Entidad', 'title': 'Puesto' },
+        { 'name': 'Entidad', 'title': 'Puesto' }
+      ]
+    };
+
+    let orgchart = new OrgChart({
+      'chartContainer': `#${this.elementId}`,
+      'data' : datascource,
+      'depth': 3,
+      'toggleSiblingsResp': false,
+      'nodeContent': 'title'
+    });
+
+
+    this.set('chart', orgchart);
+  }
+});
