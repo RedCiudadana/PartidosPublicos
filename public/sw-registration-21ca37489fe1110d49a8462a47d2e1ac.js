fbce6e94ad4c819229f64c1dca20ba17f24b77b9
d3h <diegoeauyonh@gmail.com>
2020-05-26 14:58:17 -0600

    Reload after remove service worker

--- public/sw-registration-21ca37489fe1110d49a8462a47d2e1ac.js
+++ public/sw-registration-21ca37489fe1110d49a8462a47d2e1ac.js
@@ -7,6 +7,7 @@
       (registration.waiting ? '  .waiting.scriptURL = ' + registration.waiting.scriptURL + '\n' : '') +
       (registration.active ? '  .active.scriptURL = ' + registration.active.scriptURL + '\n' : '') +
       '  .scope: ' + registration.scope + '\n'
-    )
+    );
+    window.location.reload();
   }));
 })();
