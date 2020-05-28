fc62b91560a3ec01ce7565cd7ba2a88b7a3c81a2
d3h <diegoeauyonh@gmail.com>
2020-05-28 12:14:33 -0600

    Remove css lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   "**/*.js": ["eslint --fix", "git add"],
 
-  "app/styles/**/*.{scss,css}": ["stylelint --fix", "git add"],
+  // "app/styles/**/*.{scss,css}": ["stylelint --fix", "git add"],
 
   "app/templates/**/*.hbs": "ember-template-lint"
 };
