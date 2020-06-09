34944174eafc7ebf4293716001837e92d8575174
d3h <diegoeauyonh@gmail.com>
2020-06-09 13:09:53 -0600

    Remove unused icons from font-awesome
    ember-social-share ws importing all icons from font-awesome, specifically free-brands-svg-icons, free-brands-svg-icons.
    
    I fork ember-social-share and update @fortawesome/ember-fontawesome as I
    can say in `config/icons.js` what icons include.

---
+++ config/icons.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+  return {
+    'free-brands-svg-icons': [
+      'facebook-square',
+      'twitter'
+    ],
+    'free-solid-svg-icons': [
+      'heart',
+      'users',
+      'envelope',
+      'sort-down',
+      'sort-up',
+      'sort'
+    ]
+  };
+};
