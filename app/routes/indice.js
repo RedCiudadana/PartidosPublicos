7557de0bc52828f3fe2a150070e45be2103e08c5
d3h <diegoeauyonh@gmail.com>
2020-07-30 12:02:19 -0600

    Actualizar indice

--- app/routes/indice.js
+++ app/routes/indice.js
@@ -1,4 +1,20 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndiceRoute extends Route {
+  @service
+  spreadsheets;
+
+  model() {
+    return this.spreadsheets.fetch('indice').then((response) => {
+      return response.map((indice) => {
+        indice.IndiceNormativo = indice.IndiceNormativo.replace(',', '.');
+        indice.IndiceEstructura = indice.IndiceEstructura.replace(',', '.');
+        indice.IndiceAutoridades = indice.IndiceAutoridades.replace(',', '.');
+        indice.IndicePrincipios = indice.IndicePrincipios.replace(',', '.');
+        indice.IndiceFinanciamiento = indice.IndiceFinanciamiento.replace(',', '.');
+        return indice;
+      })
+    });
+  }
 }
